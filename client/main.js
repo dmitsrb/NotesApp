ed14d8bc52aae9de3c0f631293d4050dd02ae72b
dmitsrb <dmitsrb@gmail.com>
2017-02-28 19:08:05 +0200

    some changes

--- client/main.js
+++ client/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by Dmitry on 2/28/2017.
  */
+
+/**The main file of my application. Entry point to all application**/
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
--- dist/client/main.js
+++ dist/client/main.js
@@ -1,6 +1,18 @@
-"use strict";
-
 /**
  * Created by Dmitry on 2/28/2017.
  */
+
+/**The main file of my application. Entry point to all application**/
+
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
 //# sourceMappingURL=main.js.map
